86f54c8503711c7bddd40a924cb3aaf96036bd3f
Forrest Trepte <forrest@microsoft.com>
2020-09-23 08:29:12 -0700

    C# console project

---
+++ CSharpConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
