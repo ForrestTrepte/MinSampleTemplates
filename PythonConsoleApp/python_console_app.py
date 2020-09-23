ff9e024d6c5f83e93ddb4b4569c9cda345d8c341
Forrest Trepte <forrest@microsoft.com>
2020-09-23 09:39:19 -0700

    numpy and pandas

--- PythonConsoleApp/python_console_app.py
+++ PythonConsoleApp/python_console_app.py
@@ -1,4 +1,14 @@
+import numpy as np
+import pandas as pd
+
 def double(a):
     return a * 2
+
+a = np.array([2, 3, 4])
+print(a)
+
+df = pd.DataFrame({'Name', ['cat', 'human'],
+                   'Legs', [4, 2]})
+print(df)
 
 print("Done.")
