7069377d83a314c1bdc084aa47576e0651da61a1
Forrest Trepte <forresttrepte+github@hotmail.com>
2023-11-27 10:22:02 -0800

    verify simple example outside notebook

---
+++ LangChain/simple.py
@@ -0,0 +1,13 @@
+import os
+from langchain.chat_models import ChatOpenAI
+
+# In order to make it easy to run work projects and personal AI experiments, override OPENAI_API_KEY with the value of OPENAI_API_KEY_PERSONAL if it is set.
+if "OPENAI_API_KEY_PERSONAL" in os.environ:
+    print("Using key from OPENAI_API_KEY_PERSONAL environment variable")
+    os.environ["OPENAI_API_KEY"] = os.environ["OPENAI_API_KEY_PERSONAL"]
+
+llm = ChatOpenAI(temperature=0.0, model_name="gpt-4")
+result = llm.predict(
+    "What would be a good company name for a company that makes colorful socks?"
+)
+print(result)
