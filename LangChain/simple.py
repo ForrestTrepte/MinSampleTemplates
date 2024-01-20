dc136d66f8ff98540741e34013ad51f3ce1b400d
Forrest Trepte <forresttrepte+github@hotmail.com>
2024-01-19 21:50:51 -0800

    update to current version of langchain

--- LangChain/simple.py
+++ LangChain/simple.py
@@ -1,5 +1,5 @@
 import os
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 # In order to make it easy to run work projects and personal AI experiments, override OPENAI_API_KEY with the value of OPENAI_API_KEY_PERSONAL if it is set.
 if "OPENAI_API_KEY_PERSONAL" in os.environ:
@@ -7,7 +7,7 @@ if "OPENAI_API_KEY_PERSONAL" in os.environ:
     os.environ["OPENAI_API_KEY"] = os.environ["OPENAI_API_KEY_PERSONAL"]
 
 llm = ChatOpenAI(temperature=0.0, model_name="gpt-4")
-result = llm.predict(
+result = llm.invoke(
     "What would be a good company name for a company that makes colorful socks?"
 )
-print(result)
+print(result.content)
